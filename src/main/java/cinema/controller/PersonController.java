8c71b05a3337d97b9f1b8cb26e202a3d836efc9d
Pandaluu <luciie.30@live.fr>
2020-01-24 14:13:52 +0100

    fix issues

--- src/main/java/cinema/controller/PersonController.java
+++ src/main/java/cinema/controller/PersonController.java
@@ -1,29 +1,5 @@
 package cinema.controller;
 
-import java.util.List;
-
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-
-import cinema.persistance.entity.Person;
-
 public class PersonController {
-	
-	@GetMapping
-	public List<Person> addPerson (Person person) {
-		//TODO
-		return person;
-	}
-	
-=======
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> 9db3fe00e3c3902d492bbc9bfaa1990a49f1ce5f
 
-import cinema.persistance.entity.Person;
-
-public class PersonController {
-	
-	@PostMapping
-	List<Person> addPerson (Person person) {
-		//TODO
-		return person;
+}
